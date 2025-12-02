d0a41e5da433cbbf9ec8bc25aaf4a597c675fe08
YuriyNik156 <yuriy.nik156@gmail.com>
2025-12-02 15:23:18 +0300

    add class section

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.hero {
+    width: 100%;
+    min-height: 100%;
+    background: #D7E0EB;
+    margin: 0;
+    position: relative;
+}
+
+@media (max-width: 480px) {
+  .hero {
+    width: 100%;
+    min-height: 100%;
+  }
+}
